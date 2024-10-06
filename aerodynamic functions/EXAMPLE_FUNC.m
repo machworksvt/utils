7cf31d065d7808f32890b11a370b7bb226db3514
nmullins11 <nmullins11@vt.edu>
2024-10-05 21:26:40 -0400

    Example Function

---
+++ aerodynamic functions/EXAMPLE_FUNC.m
@@ -0,0 +1,10 @@
+function [outputArg1,outputArg2] = EXAMPLE_FUNC(inputArg1,inputArg2)
+%Author
+    %Date Lasted Edited
+    %What function is this
+    %Describe the aerodynamic equations used
+    %DO NOT write your own constants use utils class constants
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
